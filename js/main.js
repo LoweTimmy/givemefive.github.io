070b6e471ddb4b58aed4080f046abad0f26b357c
LoweTimmy <947875760@qq.com>
2017-08-08 10:43:30 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 function turnper(){
-	q=document.getElementsByClassName("change");
-	q[1].style.display="none";
-	w=document.getElementsByClassName("top-name");
-	w[0].innerHTML="客服Taylor";
-	e=document.getElementsByClassName("kefu-pic");
-	e[0].style.display="block";
-	var x=document.getElementsByClassName("chat-one");
-	var y=document.getElementsByClassName("chat-two");
-	var m=document.getElementsByClassName("panel");
-	var n=document.getElementsByClassName("chat-input");
-	x[5].style.display="block";
-	m[0].scrollTop=m[0].scrollHeight;
+	alert("当前客服不在线！")
 }
